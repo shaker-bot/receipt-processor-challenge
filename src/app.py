756e350ae4dbe2a07f591f1d9b8dab1caef1752e
Abhishek Mathews <admathews1995@hotmail.com>
2024-09-26 16:18:26 -0400

    removing comments on the rules since they're in the readme

--- src/app.py
+++ src/app.py
@@ -71,32 +71,24 @@ async def get_points(id: str):
 
     points = 0
 
-    # Rules
-    # One point for every alphanumeric character in the retailer name
+    # Point Rules
     points += sum(c.isalnum() for c in receipt.retailer)
 
-    # 50 points if the total is a round dollar amount with no cents
     if receipt.total.endswith('.00'):
         points += 50
 
-    # 25 points if the total is a multiple of 0.25
     if float(receipt.total) % 0.25 == 0:
         points += 25
 
-    # 5 points for every two items on the receipt
     points += (len(receipt.items) // 2) * 5
 
-    # If the trimmed length of the item description is a multiple of 3, 
-    # multiply the price by 0.2 and round up to the nearest integer
     for item in receipt.items:
         if len(item.shortDescription.strip()) % 3 == 0:
             points += int(float(item.price) * 0.2 + 0.99)
 
-    # 6 points if the day in the purchase date is odd
     if int(receipt.purchaseDate.split('-')[2]) % 2 == 1:
         points += 6
 
-    # 10 points if the time of purchase is after 2:00pm and before 4:00pm
     purchase_time = datetime.strptime(receipt.purchaseTime, "%H:%M")
     if 14 <= purchase_time.hour < 16:
         points += 10
